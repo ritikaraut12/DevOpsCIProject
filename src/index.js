ff20406e3d900f6c1f076c62321081d0ac189b5f
ritikaraut12 <rraut1@my.centennialcollege.ca>
2025-04-14 11:50:13 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 function greet(name) {
-  return Hello, ${name}!;
+  return `Hello, ${name}!`;
 }
+
 // ✅ New function: validate email
 function validateEmail(email) {
   return /\S+@\S+\.\S+/.test(email);
 }
+
 module.exports = { greet, validateEmail };
+
 
 
 // Added email validation – Ritika
